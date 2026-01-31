7805f9e95822a666f2ae0cdce61b12f5e350c082
inin <3597536027@qq.com>
2026-01-31 17:48:50 +0800

    实现随机推荐

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
-
+#define MAX_NAME_LENGTH 50
+#define DATA_FILE "data.txt"
+#define MAX_FOODS 1000
 void printWelcome() {
     const char *welcome_art[] = {
         "            __                __           ___                       ",
@@ -32,15 +35,61 @@ void printWelcome() {
         printf("%s\n", welcome_art[i]);
     }
 }
+enum Category{
+    CHINESE,
+    FAST,
+    WESTERN,
+    JAPANESE,
+    KOREAN,
+    SNACK,
+    OTHERS,
+};
+typedef struct  {
+    char name[MAX_NAME_LENGTH];
+    enum Category category;
+    float spicy_level;
+}Food;
+Food foods[MAX_FOODS];
+int food_count = 0;
+ void random_recommend() {
+     if (food_count<=0) {
+         printf("出错啦:( 请先添加食物\n");
+         return;
+     }
+     int random_number = rand() % food_count;
+     Food food = foods[random_number];
+     printf("%s %.1f\n", food.name,food.spicy_level);
+ }
+void load_data () {
+    FILE *fp;
+    fp = fopen (DATA_FILE, "r");
+    int food_flag = 0;
+    if (fp != NULL) {
+       while (food_count<MAX_FOODS){
+            Food food;
+            food_flag=fscanf(fp, "%49[^|]|%d|%f\n", food.name,(int*)&food.category, &food.spicy_level);
+
+           if (food_flag >0) {
+               printf("%s %d %.1f\n",food.name,food.category,food.spicy_level);
+               foods[food_count]=food;
+               food_count++;
+           }else {
+               break;
+           }
+        }
+    }
+}
+
+
 
 int main(void) {
     // Windows console UTF-8 setup
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
-
+     srand((unsigned int)time(NULL));
     printWelcome();
-
+    load_data ();
     printf("主菜单：\n1. 随机推荐\n2. 管理食物\n3. 查看历史\n4. 按分类筛选\n5. 清空历史\n6. 退出\n");
     printf("请输入序号选择你的操作：\n");
     char choice;
@@ -50,7 +99,7 @@ int main(void) {
         printf("%c ", choice);
         switch (choice) {
             case '1':
-                printf("随机推荐\n");
+                random_recommend();
                 break;
             case '2':
                 printf("管理食物\n");
